75f77fc4e9c5a8cfc481ca8da01b95346d487528
sattya19 <sattyasharma195@gmail.com>
2023-09-11 13:11:18 +0530

    sumOfDigits

---
+++ sumOfDigits.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class sumOfDigits {
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+        int num;
+        num = sc.nextInt();
+        sc.close();
+
+        sum(num);
+
+    }
+
+    public static int sum(int num) {
+
+        int sumOfnum = 0;
+        while (num > 0) {
+            int rem = num % 10;
+            sumOfnum = rem + sumOfnum;
+            num = num / 10;
+        }
+        System.out.print(sumOfnum);
+        return sumOfnum;
+    }
+
+}
