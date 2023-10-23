a071f8292d348fba7379455bf3913bd73eacc84a
sattya19 <sattyasharma195@gmail.com>
2023-10-23 22:54:33 +0530

    addedReverseArray

---
+++ Array/reverseArray.java
@@ -0,0 +1,31 @@
+package Array;
+
+public class reverseArray {
+    public static void reverse(int[] num) {
+        int first = 0;
+        int last = num.length - 1;
+
+        while (first < last) {
+            int temp = num[last];
+            num[last] = num[first];
+            num[first] = temp;
+
+            first++;
+            last--;
+
+        }
+
+    }
+
+    public static void main(String args[]) {
+        int num[] = { 2, 4, 5, 6 };
+
+        reverse(num);
+        for (int i = 0; i < num.length; i++) {
+            System.out.print(num[i]);
+            System.out.println();
+        }
+
+    }
+
+}
